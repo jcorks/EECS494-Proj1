6f4639122567cd952061a92f2d3db1dbd8bea3d0
patrshin <patrshin@umich.edu>
2015-01-22 19:21:55 -0500

    working on red arremer

---
+++ Ghost n' Goblins/Assets/Scripts/RedArremer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedArremer : MonoBehaviour {
+
+	bool awaken;
+	bool grounded;
+	bool hover;
+
+	// Use this for initialization
+	void Start () {
+		awaken = false;
+		grounded = false;
+		hover = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Weapon" && GetComponent<Enemy>().ready) {
+			if (!awaken) {
+				awaken = true;
+				grounded = true;
+				print ("awakened");
+			}
+			if (
+		}
+		
+		if (other.gameObject.GetComponent<Arthur> ()) {
+			print ("Hello, arthur!");
+		}
+	}
+}
