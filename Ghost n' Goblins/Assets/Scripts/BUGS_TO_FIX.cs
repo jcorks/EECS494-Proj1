6d5befc93380ff5ad0851ac4815399ebab6a27ed
jcorks <jcorkery@umich.edu>
2015-01-28 00:56:23 -0600

    Added bugs script

---
+++ Ghost n' Goblins/Assets/Scripts/BUGS_TO_FIX.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BUGS_TO_FIX : MonoBehaviour {
+
+	/*
+*JC:
+* 	- jumping over tombstones occurs only 75% of the time it should.
+* 	- Work on unicorn logic some more
+* 
+*PS:
+* 	- Fireball still falls through floor?
+* 	- One weapon can kill multiple enemies in range?
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+	 */
+}
