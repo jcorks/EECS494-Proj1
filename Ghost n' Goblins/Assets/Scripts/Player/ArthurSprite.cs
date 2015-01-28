e301f0d32b783d85f8b1dcf0e6d1664a085f87ca
patrshin <patrshin@umich.edu>
2015-01-27 20:08:12 -0500

    working on sprite. now switch sides, climbs, jumps, and moves

--- Ghost n' Goblins/Assets/Scripts/Player/ArthurSprite.cs
+++ Ghost n' Goblins/Assets/Scripts/Player/ArthurSprite.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class ArthurSprite : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public void Change(Sprite what) {
+		this.GetComponent<SpriteRenderer> ().sprite = what;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
