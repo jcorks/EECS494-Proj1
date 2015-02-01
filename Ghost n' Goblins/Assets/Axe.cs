9b6e1655178d61d294d9af43f65cdd6faab0648a
patrshin <patrshin@umich.edu>
2015-01-31 20:51:04 -0500

    Knight basically done

---
+++ Ghost n' Goblins/Assets/Axe.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Axe : MonoBehaviour {
+	
+	public float speed = 2.5f;
+	public float side;
+	public float start;
+	
+	// Use this for initialization
+	void Start () {
+		start = transform.position.x;
+		Vector3 temp = transform.position;
+		temp.x += side;
+		Vector2 dirVec =  temp - transform.position; 
+
+		GetComponent<PhysObj> ().addVelocity (dirVec * speed);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x - start > 30 || transform.position.x - start < -30)
+			Destroy (this.gameObject);
+	}
+}
---
+++ Ghost n' Goblins/Assets/Knight.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knight : MonoBehaviour {
+	
+	Vector3 pos;
+	public GameObject axePrefab;
+
+	public float side;
+	public float fireRate = 3.5f;
+	float variation;
+	float fireCount = 0;
+	float speed; 
+	Vector3 origin;
+	float chanceToChangeDirection = 0.005f;
+	float range = 2f;
+	float up;
+
+	// Use this for initialization
+	void Start () {
+		up = 1f;
+		origin = transform.position;
+		speed = 0.008f;
+		variation = Random.Range(0f,1.5f);
+	}
+
+	void FixedUpdate() {
+		if (Random.Range (0f,1f) < chanceToChangeDirection) {
+			speed *= -1f; //Change direction randomly
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += speed;
+		fireCount += Time.deltaTime;
+		transform.position = pos;
+		if (transform.position.x > range+origin.x || transform.position.x < -range+origin.x) {
+			speed *= -1f;
+		}
+		if (fireCount > fireRate + variation) {
+			Debug.Log("AXING");
+			launchProjectile();
+			fireCount = 0f;
+			variation = Random.Range(0f,1.5f);
+		}
+		// Only hitable if arthur is behind
+		if (Arthur.weapon != WeaponType.XBOW) {
+			if (side == -1f)
+				GetComponent<Enemy> ().ignoreProjectiles = (Arthur.arthurPos.x < transform.position.x);
+			else if (side == 1f)
+				GetComponent<Enemy> ().ignoreProjectiles = (Arthur.arthurPos.x > transform.position.x);
+		}
+	}
+
+	void launchProjectile() {
+
+		GameObject axe = (GameObject)Instantiate (axePrefab);
+		Vector3 pos = transform.position;
+		if (up == 1f)
+			pos.y -= 0.4f;
+		else
+			pos.y += 0.1f;
+		axe.transform.position = pos;
+		axe.GetComponent<Axe> ().side = side;
+		up *= -1f;
+		
+		
+	}
+}
