8f03cd535e3ce6d598f9e4d48c8d5d3e95989f23
jcorks <jcorkery@umich.edu>
2015-02-04 02:59:34 -0600

    Finished (?) pegasus satan

--- Ghost n' Goblins/Assets/FinalBossManager.cs
+++ Ghost n' Goblins/Assets/FinalBossManager.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class FinalBossManager : MonoBehaviour {
 
 	static public Vector3 basePos;
+	static public bool defeated = false;
+	public GameObject rock;
+	float alpha = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +16,21 @@ public class FinalBossManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate() {
+		if (defeated) {
+			Manager.shakeCamera(.1f);
+			if (Random.value < .1) {
+				GameObject n = (GameObject)Instantiate(rock);
+				n.transform.position = Arthur.arthurPos + new Vector3(Random.Range (-7, 7), 6, 0);
+				n.GetComponent<Enemy>().ready = false;
+			}
+			alpha+=.002f;
+			GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, alpha);
+		}
+
+		if (alpha > 1.2f)
+						Application.LoadLevel ("congrats");
 	}
 }
--- Ghost n' Goblins/Assets/Scripts/Enemies/Pegasus.cs
+++ Ghost n' Goblins/Assets/Scripts/Enemies/Pegasus.cs
@@ -9,36 +9,43 @@ public class Pegasus : MonoBehaviour {
 	public float ArenaXMin;
 	public float ArenaXMax;
 	public GameObject platform;
+	public GameObject movingPlatform;
+	public GameObject item;
 	
 	Enemy thisE;
 	PhysObj thisPhys;
 	SpriteRenderer spr;
+	public Sprite originalSpr;
+	public Sprite invertedSpr;
 
 	
 	float initWaitTime 		= 3f;
 	float chanceToGoFast 	= .006f;
 	float chanceToChangeDir = .002f;
-	float chanceToFire      = .005f;
+	float chanceToFire      = .004f;
 	float fastDuration 		= 4f; // durtion for fastness in seconds
 	float shotDuration   	= 1f;
-	float freezeDuration    = .1f;
+	float freezeDuration    = .6f;
 	float shotSpeed         = 6f;
-	float minimumFireTime   = 4.5f; 
+	float minimumFireTime   = 3.5f; 
 	int   numRocks          = 7;
-	Vector3 shotOrigin		= new Vector3(0, .1f, 0f);
+	Vector3 shotOrigin		= new Vector3(0, .4f, 0f);
 	
 	
 	float timeSinceStarted	= 0f;
 	float timeFastLeft      = EXPIRED;
 	float timeShotLeft      = EXPIRED;
 	float timeShotSince     = 6f;
-	float timeFrozen        = 0f;
+	float timeFrozen        = -1f;
 	Vector2 frozenVel		= new Vector2(0, 0);
 	int   rocksLaunched     = 0;
+	float chanceToRock		= .2f;
 	
 	float speed				= 2f;
 	float fastSpeed 		= 3f;
-	float jumpVelocity		= 20f;
+	float jumpVelocity		= 24f;
+	bool isDoingBigJump = false;
+	bool shotsFired = false;
 
 
 	
@@ -55,21 +62,42 @@ public class Pegasus : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (timeFrozen > 0) {
+			thisPhys.setVelocity(new Vector2(0,thisPhys.getVelocity().y));
+			thisE.ignoreProjectiles = true;
+			if (spr.sprite == originalSpr) {
+				spr.sprite = invertedSpr;
+			} else {
+				spr.sprite = originalSpr;
+			}
+			timeFrozen-=Time.deltaTime;
+			if (	timeFrozen < 0) {
+				thisE.ignoreProjectiles = false;
+				spr.sprite = originalSpr;
+
+			}
+		}
+
+		if (isDoingBigJump) {
+			if (thisPhys.getVelocity().y < 0) {
+				isDoingBigJump = false;
+				thisPhys.ignoreGround = false;
+			}
+		}
 		
-		thisE.ready = GetComponent<MeshRenderer>().isVisible || started;
+		thisE.ready = (GetComponent<MeshRenderer>().isVisible || started) && !isDoingBigJump;
 		initWait ();
 		
 		timeSinceStarted += Time.deltaTime;
 		timeFastLeft -= Time.deltaTime;
 		timeShotLeft -= Time.deltaTime;
-
+		timeShotSince -= Time.deltaTime;
 
 		
-		if (!thisE.ready || !started || timeShotLeft > 0)
+		if (!thisE.ready || !started || (timeShotLeft > 0) || isDoingBigJump)
 			return;
 
-
-		timeShotSince -= Time.deltaTime;
 
 
 		if (thisPhys.isGrounded && (Random.value <= chanceToFire || timeShotSince <0)) {
@@ -82,13 +110,7 @@ public class Pegasus : MonoBehaviour {
 
 
 		if (timeFrozen > 0) {
-			thisPhys.setVelocity(new Vector2(0,0));
-			timeFrozen-=Time.deltaTime;
-			if (	timeFrozen < 0) {
-				thisPhys.setVelocity (frozenVel);
-			}
 			return;
-
 		}
 		
 		// reset speed if fastness expired
@@ -133,14 +155,15 @@ public class Pegasus : MonoBehaviour {
 		rocksLaunched = 0;
 
 
-		if (thisE.health < 10) {
+		if (thisE.health < 10 && (Random.value < .3)) {
 			GameObject fb = (GameObject)Instantiate (FireballPrefab);
-
+			shotsFired = true;
 
 
 			fb.GetComponent<PhysObj> ().setVelocity (new Vector2 (getDir () * shotSpeed, 0));
 			fb.transform.position = transform.position + shotOrigin;
 		}
+
 		
 	}
 	
@@ -161,18 +184,18 @@ public class Pegasus : MonoBehaviour {
 	
 	
 	void FixedUpdate() {
-		if (!started)
+		if (!started || isDoingBigJump)
 			return;
 
-		if (timeShotLeft > 0) {
+		if (timeShotLeft > 0 && !shotsFired) {
 			if (!thisPhys.isGrounded) return;
 			Manager.shakeCamera (.1f);
-			if (Random.value < .2 && rocksLaunched++ < numRocks) {
+			if (Random.value < chanceToRock && rocksLaunched++ < numRocks) {
 				GameObject n = (GameObject)Instantiate(rockPrefab);
 				n.transform.position = new Vector3(Arthur.arthurPos.x + Random.value*12 - 6, Arthur.arthurPos.y+6f	, 0);
 			}
 			return;
-		}
+		} else if (timeShotLeft < 0 && shotsFired) {shotsFired = false;}
 		
 		if (Random.value <= chanceToChangeDir || !GetComponent<MeshRenderer>().isVisible) {
 			thisPhys.setVelocity(
@@ -199,31 +222,64 @@ public class Pegasus : MonoBehaviour {
 	
 	
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Weapon") {
+		if (other.tag == "Weapon" && (timeFrozen <= 0) && !isDoingBigJump) {
+			timeFrozen = freezeDuration;
+
 			if (thisE.health % 10 == 0 && thisE.health < 30) {
+				print ("Health Left:" + thisE.health.ToString());
 				thisPhys.setVelocity (new Vector2(0, jumpVelocity));
+				isDoingBigJump = true;
+				thisPhys.ignoreGround = true;
 			} else {
 				if (thisPhys.isGrounded && timeShotLeft < 0) {
-					thisPhys.setVelocity (new Vector2(0, 10));
-					timeFrozen = freezeDuration;
+					thisPhys.setVelocity (new Vector2(0, 7));
+
 					frozenVel = thisPhys.getVelocity ();	
 				}
 			}
 
-
-
 			if (thisE.health == 20) {
+				chanceToFire+= .01f;
+				GameObject i = (GameObject) Instantiate (item); 
+				i.GetComponent<Items>().item = ItemType.ARMOR;
+				i.transform.position = FinalBossManager.basePos + new Vector3(0, 3f, 0);
 				GameObject n = (GameObject) Instantiate (platform);
 				n.transform.position = FinalBossManager.basePos + new Vector3(0, 4.5f, 0);
 				n = (GameObject) Instantiate (platform);
 				n.transform.position = FinalBossManager.basePos + new Vector3(0, 6.5f, 0);
-
+				
+				
 			}
+			
+			if (thisE.health == 10) {
+				
+				
+				GameObject i = (GameObject) Instantiate (item); 
+				i.GetComponent<Items>().item = ItemType.ARMOR;
+				i.transform.position = FinalBossManager.basePos + new Vector3(-2.04f, 10f, 0);
+				GameObject n = (GameObject) Instantiate (movingPlatform);
+				n.transform.position = FinalBossManager.basePos + new Vector3(-2.04f, 11.51f, 0);
+				n = (GameObject) Instantiate (movingPlatform);
+				n.transform.position = FinalBossManager.basePos + new Vector3(2.04f, 13.75f, 0);
+				n.GetComponent<MovingPlatform>().speed = 1.5f;
+			}
+			
+			// ROCKIN MODE
+			if (thisE.health == 7) {
+				numRocks = 8;
+				chanceToFire = .06f;
+				shotDuration = .8f;
+				chanceToRock = 1f;
+			}
+
+
+
 		}
 	}
 	
 	void OnDestroy() {
-		Manager.beatLevel = true;
+
+		FinalBossManager.defeated = true;
 	}
 
 	float getDir() {
